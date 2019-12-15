10b3e1c06abff7e918b354b3aca6b3acf23c1b1b
Sophia Tannir <stannir@smith.edu>
2019-12-15 17:12:36 -0500

    updating tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,6 @@ test_that("data correct", {
   expect_identical(game$home_team, na.omit(game$home_team))
   expect_identical(game$away_team, na.omit(game$away_team))
   expect_equal(ncol(player), 4)
+  expect_s3_class(goalkeeper_season_stats, "data.frame")
 }
 )
